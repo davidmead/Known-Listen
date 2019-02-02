f06241bce75ecd8f6318eb998ad7a7ae1d6fc49e
David Mead <davidjohnmead@gmail.com>
2019-02-02 17:06:04 -0500

    more cleanup
    * removed whitespace
    * added `.form-group` Bootstrap class to `<p>`

--- templates/default/entity/Listen/edit.tpl.php
+++ templates/default/entity/Listen/edit.tpl.php
@@ -37,69 +37,44 @@
 
 ?>
     <form action="<?= $vars['object']->getURL() ?>" method="post" enctype="multipart/form-data">
-
         <div class="row">
-
             <div class="col-md-8 col-md-offset-2 edit-pane">
-
-
-                <?php
-
-                    if (empty($vars['object']->_id)) {
-
-                        ?>
-                        <h4>What did you listen to?</h4>
-                    <?php
-
-                    } else {
-
-                        ?>
-                        <h4>Edit what you listened to</h4>
-                    <?php
-
-                    }
-
-                ?>
-
-                <?php
-
-                    if (empty($vars['object']->_id) || empty($vars['object']->getAttachments())) {
-
-                        ?>
-                        <div id="photo-preview"></div>
-                        	<p><span class="btn btn-primary btn-file"><i class="fa fa-camera"></i> <span id="photo-filename">Select a image</span> <input type="file" name="photo" id="photo" class="col-md-9 form-control" accept="image/*;capture=camera" onchange="photoPreview(this)"/></span></p>
-
-                    <?php
-
-                    }
-
-                ?>
+                <?php if (empty($vars['object']->_id)) { ?>
+                <h4>What did you listen to?</h4>
+                <?php } else { ?>
+                <h4>Edit what you listened to</h4>
+                <?php } ?>
+                <?php if (empty($vars['object']->_id) || empty($vars['object']->getAttachments())) { ?>
+                <div id="photo-preview"></div>
+                <p><span class="btn btn-primary btn-file"><i class="fa fa-camera"></i> <span id="photo-filename">Select a image</span> <input type="file" name="photo" id="photo" class="col-md-9 form-control" accept="image/*;capture=camera" onchange="photoPreview(this)"/></span></p>
+				<?php } ?>
                 <div class="content-form">
-                    <p><label for="title">Title</label>
-                    <input type="text" name="title" id="title" placeholder="Title of the song, podcast, album, etc." value="<?= htmlspecialchars($title) ?>" class="form-control"/></p>
-
-                    <p><label for="mediaURL">Media Link</label>
-                    <input type="text" name="mediaURL" id="mediaURL" placeholder="Link to the song, podcast, album, etc." value="<?= htmlspecialchars($mediaURL) ?>" class="form-control"/></p>
-
-                    <p>
-                    <label for="listenType">Type</label>
-<select class="form-control" name="listenType" id="listenType">
-  <option data-listenType="song" value="song" <?php if ($listenType == 'song' ) echo 'selected' ; ?>>song</option>
-  <option data-listenType="album" value="album" <?php if ($listenType == 'album' ) echo 'selected' ; ?>>album</option>
-  <option data-listenType="soundtrack" value="soundtrack" <?php if ($listenType == 'soundtrack' ) echo 'selected' ; ?>>soundtrack</option>
-  <option data-listenType="stream" value="stream" <?php if ($listenType == 'stream' ) echo 'selected' ; ?>>stream</option>
-  <option data-listenType="podcast" value="podcast" <?php if ($listenType == 'podcast' ) echo 'selected' ; ?>>podcast</option>
-  <option data-listenType="audiobook" value="audiobook" <?php if ($listenType == 'audiobook' ) echo 'selected' ; ?>>audio book</option>
-</select></p>
-
-                    <p><label for="listenAuthor">Artist</label>
-                    <input type="text" name="listenAuthor" id="listenAuthor" placeholder="Who is the artist?" value="<?= htmlspecialchars($listenAuthor) ?>" class="form-control"/></p>
-
-
-                </div>
-
-                <p><label for="body">Summary</label>
-                <?= $this->__([
+                	<p class="form-group">
+	                	<label for="title">Title</label>
+                    	<input type="text" name="title" id="title" placeholder="Title of the song, podcast, album, etc." value="<?= htmlspecialchars($title) ?>" class="form-control"/>
+                    </p>
+                    <p class="form-group">
+	                    <label for="mediaURL">Media Link</label>
+						<input type="text" name="mediaURL" id="mediaURL" placeholder="Link to the song, podcast, album, etc." value="<?= htmlspecialchars($mediaURL) ?>" class="form-control"/>
+					</p>
+                    <p class="form-group">
+	                    <label for="listenType">Type</label>
+						<select class="form-control" name="listenType" id="listenType">
+							<option data-listenType="song" value="song" <?php if ($listenType == 'song' ) echo 'selected' ; ?>>song</option>
+							<option data-listenType="album" value="album" <?php if ($listenType == 'album' ) echo 'selected' ; ?>>album</option>
+							<option data-listenType="soundtrack" value="soundtrack" <?php if ($listenType == 'soundtrack' ) echo 'selected' ; ?>>soundtrack</option>
+							<option data-listenType="stream" value="stream" <?php if ($listenType == 'stream' ) echo 'selected' ; ?>>stream</option>
+							<option data-listenType="podcast" value="podcast" <?php if ($listenType == 'podcast' ) echo 'selected' ; ?>>podcast</option>
+							<option data-listenType="audiobook" value="audiobook" <?php if ($listenType == 'audiobook' ) echo 'selected' ; ?>>audio book</option>
+						</select>
+					</p>
+                    <p class="form-group">
+	                    <label for="listenAuthor">Artist</label>
+						<input type="text" name="listenAuthor" id="listenAuthor" placeholder="Who is the artist?" value="<?= htmlspecialchars($listenAuthor) ?>" class="form-control"/>
+					</p>
+					<p class="form-group">
+						<label for="body">Summary</label>
+						<?= $this->__([
                     'name' => 'body',
                     'value' => $body,
                     'object' => $object,
@@ -110,18 +85,15 @@
                 <?php if (empty($vars['object']->_id)) echo $this->drawSyndication('article'); ?>
                 <?php if (empty($vars['object']->_id)) { ?><input type="hidden" name="forward-to" value="<?= \Idno\Core\site()->config()->getDisplayURL() . 'content/all/'; ?>" /><?php } ?>
 
-                <?= $this->draw('content/access'); ?></p>
-
+                <?= $this->draw('content/access'); ?>
+                	</p>
+				</div>
                 <p class="button-bar ">
-
                     <?= \Idno\Core\site()->actions()->signForm('/listen/edit') ?>
                     <input type="button" class="btn btn-cancel" value="Cancel" onclick="tinymce.EditorManager.execCommand('mceRemoveEditor',false, 'body'); hideContentCreateForm();"/>
                     <input type="submit" class="btn btn-primary" value="Publish"/>
-
                 </p>
-
             </div>
-
         </div>
     </form>
 
